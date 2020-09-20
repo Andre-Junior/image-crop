66433d6c8d789557fcef3f84d240ecc4ec8ad9b5
Andre-Junior <andrejunior.dias@gmail.com>
2020-09-20 14:32:18 -0300

    Selection Tool
    Seleciton tool finalizada

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const photoFile = document.getElementById('photo-file')
+let image = document.getElementById('photo-preview')
 
 //Select & Preview image
 
@@ -13,7 +14,6 @@ window.addEventListener('DOMContentLoaded', () => {
         let reader = new FileReader()
         reader.readAsDataURL(file)
         reader.onload = (event) => {
-            let image = document.getElementById('photo-preview')
             image.src = event.target.result
         }
     })
@@ -21,11 +21,49 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //Selection tooll
 
+const selection=  document.getElementById('selection-tool')
+
+let startX, startY, relativeStartX, relativeStartY,
+endX, endY, relativeEndX, relativeEndY;
+let startSelection = false;
+
 const events = {
-    mouseover(){},
-    mousedown(){},
-    mousemove(){},
-    mouseup(){} 
+    mouseover(){
+        this.style.cursor = 'crosshair'
+    },
+    mousedown(){
+        const {clientX, clientY, offsetX, offsetY} = event
+
+        startX = clientX
+        startY = clientY
+        relativeStartX = offsetX
+        relativeStartY = offsetY
+
+        startSelection = true
+    },
+    mousemove(){
+        endX = event.clientX
+        endY = event.clientY
+        
+        if(startSelection) {
+            selection.style.display = 'initial';
+            selection.style.top = startY + 'px';
+            selection.style.left = startX + 'px';
+        
+            selection.style.width = (endX - startX) + 'px';
+            selection.style.height = (endY - startY) + 'px';
+        }
+
+    },
+    mouseup(){
+        startSelection = false;
+
+        relativeEndX = event.layerX;
+        relativeEndY = event.layerY;
+    } 
 }
 
 Object.keys(events)
+.forEach(eventName => {
+    image.addEventListener(eventName, events[eventName])
+})
