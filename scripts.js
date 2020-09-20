29d332e155d93677486c568ca7410516a67a4b51
Andre-Junior <andrejunior.dias@gmail.com>
2020-09-20 12:47:20 -0300

    Seletor de imagem
    Seletor de imagem e aparecendo no src feito

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
+const photoFile = document.getElementById('photo-file')
+
 document.getElementById('select-image')
 .onclick= () => {
-    document.getElementById('photo-file').click()
+    photoFile.click()
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    photoFile.addEventListener('change', () => {
+        let file = photoFile.files.item(0)
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = (event) => {
+            let image = document.getElementById('photo-preview')
+            image.src = event.target.result
+        }
+    })
+})
